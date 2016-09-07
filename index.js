94ddc68f94922c04f777bce603735cc8a10ae750
Jonathan Junca <Jonathan@Jonathans-MacBook-Pro.local>
2016-09-07 09:13:21 -0600

    removed the this keyword from pubnub methods

--- index.js
+++ index.js
@@ -3,17 +3,17 @@ var homeOne = function(obj) {
   const PubNub = require('pubnub');
   const Gpio = require('onoff').Gpio;
 
-  this.pubnub = new PubNub({
+  var pubnub = new PubNub({
     subscribeKey: obj.subscribeKey,
     publishKey: obj.publishKey,
   });
 
-  this.publishMessage = function() {
+  var publishMessage = function() {
     var publishConfig = {
       channel: obj.channel,
       message: obj.title
     };
-    this.pubnub.publish(publishConfig, function(status, response) {
+    pubnub.publish(publishConfig, function(status, response) {
       if (response) { console.log("Here is the response: ", response); }
       else { console.log("Here is the status: ", status); }
     });
@@ -21,9 +21,9 @@ var homeOne = function(obj) {
 
   this.switch = function(pin) {
     var button = new Gpio(pin, 'in', 'both');
-    button.watch.call(this, function(err, value) {
+    button.watch(function(err, value) {
       if (value === 1) {
-        this.publishMessage();
+        publishMessage();
       }
     });
   };
